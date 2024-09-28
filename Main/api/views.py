4e3430c918df4f87e6429ab3bf7378d85be90066
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-09-28 13:41:14 -0300

    added obtener_usuario view and frontend app

--- Main/api/views.py
+++ Main/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import os
 
-# API views
+# API APP views
+
+ARCHIVO_JSON = os.path.join(os.path.dirname(__file__), 'data.json')
+
+def obtener_usuario(req):
+    # si existe el archivo
+    if os.path.exists(ARCHIVO_JSON):
+        # abrir el archivo como lectura
+        with open(ARCHIVO_JSON, 'r') as f:
+            # cargar los datos del archivo como json
+            datos = json.load(f)
+    else:
+        datos = []
+    # retornar los datos en formato json
+    return JsonResponse(datos, safe=False)
+
+
