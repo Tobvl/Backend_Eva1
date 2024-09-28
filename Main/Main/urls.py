95d1bdead00472d9ded6a182f076a5c15e10e764
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-09-28 13:44:33 -0300

    added Main project API APP views (endpoint /api)

--- Main/Main/urls.py
+++ Main/Main/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# importar vistas de la app api y frontend
+from api.views import obtener_usuario
+#from frontend.views import formulario <- o la vista necesaria
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # api app
+    path('api/obtener_usuario', obtener_usuario),
+    # frontend app
+    #path('', formulario),
 ]
