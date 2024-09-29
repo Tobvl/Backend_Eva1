ec7b1bcd4f90fec0680f96433bc794a80b658099
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-09-29 18:16:04 -0300

    Added login and logout endpoints

--- Main/api/urls.py
+++ Main/api/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 # urls de la app api
+app_name = 'api'
 urlpatterns = [
     path('', views.index, name='index'),
     path('usuarios/', views.obtener_usuarios, name='obtener_usuarios'),
     path('registrar/', views.registrar_usuario, name='registrar_usuario'),
+    path('login/', views.login_usuario, name='login_usuario'),
+    path('usuarios/logout/', views.logout_usuario, name='logout_usuario'),
 ]
