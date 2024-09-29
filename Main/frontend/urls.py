6584dfad3d64a19e550e159db3d20923985a20f9
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-09-29 18:20:13 -0300

    Added app name to urls to identify them in project

--- Main/frontend/urls.py
+++ Main/frontend/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+# urls de la app frontend
+app_name = 'frontend'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('registrar/', views.form_registrar, name='registrar_usuario'),
-    path('ingresar/', views.form_login, name='login'),
+    path('registrar/', views.form_registrar, name='form_registrar'),
+    path('ingresar/', views.form_login, name='form_login'),
     path('perfil/', views.perfil, name='perfil'),
 ]
